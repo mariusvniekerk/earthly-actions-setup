625b23c3e1901dfc2c6074d0d759909e49eadb28
Marius van Niekerk <marius.v.niekerk@gmail.com>
2024-07-05 15:21:24 +0000

    compile new sources

--- src/cache-utils.ts
+++ src/cache-utils.ts
@@ -22,6 +22,7 @@ export function isCacheFeatureAvailable(): boolean {
 
     return false;
   }
+
   if (core.getInput('use-cache') !== "true") { 
     core.info(`skipping cache usage as use-cache is not true`); 
     return false; 
